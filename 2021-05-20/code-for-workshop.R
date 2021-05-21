d8d06fa0eb493a6df11ad8599be1ac5b9454194d
Mauricio Vargas <mavargas11@uc.cl>
2021-05-21 16:59:54 -0400

    updated examples

--- 2021-05-20/code-for-workshop.R
+++ 2021-05-20/code-for-workshop.R
@@ -10,6 +10,10 @@ flights %>% filter(month == 3)
 
 # indexes ----
 
+airlines
+
+planes
+
 unique_index <- list(
   airlines = list("carrier"),
   planes = list("tailnum")
@@ -37,17 +41,22 @@ fun_connect <- function() {
   )
 }
 
-schema <- "public"
-
 # explore tables ----
 
 conn <- fun_connect()
+
+# this is not the best option !
+schema <- "public"
 remote_tables <- dbGetQuery(
   conn,
   sprintf("SELECT table_name
           FROM information_schema.tables
           WHERE table_schema='%s'", schema)
 )
+
+# this is the same but shorter!
+DBI::dbListTables(conn)
+
 dbDisconnect(conn)
 
 remote_tables <- as.character(remote_tables$table_name)
@@ -73,10 +82,61 @@ for (table in tables) {
 # access tables ----
 
 conn <- fun_connect()
+
 obs_per_month <- tbl(conn, "flights") %>%
   group_by(year, month) %>%
   count() %>%
   collect()
-dbDisconnect(conn)
+
+tbl(conn, "flights") %>%
+  group_by(year, month) %>%
+  count() %>%
+  show_query()
 
 obs_per_month
+
+weather %>%
+  count(year, month, day, hour, origin)
+
+tbl(conn, "weather") %>%
+  count(year, month, day, hour, origin)
+
+tbl(conn, "flights")
+
+tbl(conn, "airports")
+
+tbl(conn, "flights") %>%
+  group_by(origin) %>%
+  count() %>%
+  inner_join(tbl(conn, "airports") %>% select(faa, name), by = c("origin" = "faa"))
+
+dbDisconnect(conn)
+
+# write in batches ----
+
+N <- 12 * 10^6
+
+df <- tibble(x = runif(N), y = rnorm(N))
+
+library(tidyverse)
+
+df <- df %>%
+  mutate(p = floor(row_number() / 100000) + 1) %>%
+  group_by(p) %>%
+  nest() %>%
+  ungroup() %>%
+  select(data) %>%
+  pull()
+
+library(glue)
+
+map(
+  seq_along(df),
+  function(x) {
+    message(glue("Writing fragment { x } of { length(df) }"))
+    conn <- fun_connect()
+    dbWriteTable(conn, "df_example", df[[x]],
+                 append = TRUE, overwrite = FALSE, row.names = FALSE)
+    dbDisconnect(conn)
+  }
+)
